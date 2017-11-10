4f85e6e2dc600be7924c97c9a1d6f29d119d5a7e
Jarrett <jarrettminton00@gmail.com>
2017-11-10 08:43:39 -0700

    Add files via upload

---
+++ rookie-kit-2017/tps/__init__.py
@@ -0,0 +1,8 @@
+#
+# Don't change this file
+#
+from . import common
+from . import engine_server
+from . import client
+from . import engine_client
+
---
+++ rookie-kit-2017/tps/common/__init__.py
@@ -0,0 +1,16 @@
+#
+# Don't change this file
+#
+# ORDER MATTERS
+from .game_message import *
+from .object import *
+from .missile import *
+from .npc import *
+from .player import *
+from .wall import *
+from .event import *
+from .object_message import *
+from .command_message import *
+from .event_message import *
+from .game_comm import *
+from .game import *
